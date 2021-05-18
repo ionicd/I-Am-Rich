9d8b0b5b17035a52af2d9fb6f15e8802dc7c5ab6
saurav-kc <Saurav.KC14@myhunter.cuny.edu>
2021-05-17 23:42:26 -0400

    Initial commit

---
+++ I Am Rich/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  I Am Rich
+//
+//  Created by Saurav on 5/17/21.
+//  Copyright © 2021 SAURAV. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
